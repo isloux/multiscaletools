48776dbb9007fbf9e41a9a9e5d42c25020000591
Christophe Ramananjaona <christophe.ramananjaona@polytechnique.edu>
2022-10-27 14:52:29 +0200

    Fix function name

--- C++/joueavec.cpp
+++ C++/joueavec.cpp
@@ -10,8 +10,8 @@ int main() {
 	size_t n = 1000;
 	unique_ptr<TimeSeries<TYPE>> a = make_unique<TimeSeries<TYPE>>(n);
 	unique_ptr<TimeSeries<TYPE>> b = make_unique<TimeSeries<TYPE>>(n);
-	a->from_column_file("timeseries1.txt");
-	b->from_column_file("timeseries2.txt");
+	a->from_two_column_file("timeseries1.txt");
+	b->from_two_column_file("timeseries2.txt");
 	cout << "Moyenne de a : " << a->mean() << endl;
 	cout << "Moyenne de b : " << b->mean() << endl;
 	cout << "Covariance : " << covariance(a, b) << endl;
